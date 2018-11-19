5323e9faead0db6a812a21e5040c38eb2e1f5105
shishijie <shishijie@live.com>
2018-11-19 16:52:29 +0800

    Initial commit

---
+++ src/main/java/com/yztsoft/sosu/SosuApplication.java
@@ -0,0 +1,12 @@
+package com.yztsoft.sosu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosuApplication.class, args);
+	}
+}
