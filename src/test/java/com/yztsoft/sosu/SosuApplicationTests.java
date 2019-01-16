cfdb3c6bb92995bacdb9256acd302a50d9e90b43
shishijie <shishijie@live.com>
2019-01-16 10:35:27 +0800

    1.0 add source json

--- src/test/java/com/yztsoft/sosu/SosuApplicationTests.java
+++ src/test/java/com/yztsoft/sosu/SosuApplicationTests.java
@@ -4,7 +4,6 @@ import com.yztsoft.sosu.esclient.EsClient;
 import org.elasticsearch.action.admin.indices.alias.IndicesAliasesRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
---
+++ src/test/java/com/yztsoft/sosu/test.java
@@ -0,0 +1,12 @@
+package com.yztsoft.sosu;
+
+/**
+ * @classname: test
+ * @description:
+ * @author: Shi Shijie
+ * @create: 2019-01-07 15:35
+ **/
+
+public class test {
+
+}
